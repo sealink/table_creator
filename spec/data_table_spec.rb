14156f2c7c0fafecf0198b8e56e79684dd3f18cf
Michael Noack <michael@noack.com.au>
2017-07-25 15:00:22 +0930

    Add spec to document existing money behaviour

--- spec/data_table_spec.rb
+++ spec/data_table_spec.rb
@@ -1,14 +1,34 @@
 require 'spec_helper'
 
 describe DataTable::DataTable do
-  let(:row) { ['col1', 2] }
+  let(:row) { ['col1', 2, Money.new(3)] }
+  let(:money_class) {
+    Class.new do
+      def initialize(dollars)
+        @dollars = dollars
+      end
+
+      def format
+        "$#{with_places}"
+      end
+
+      def to_s
+        with_places
+      end
+
+      def with_places
+        '%.2f' % @dollars
+      end
+    end
+  }
 
   before do
+    stub_const 'Money', money_class
     subject << { body: [row] }
   end
 
   it 'should generate csv' do
-    expect(subject.to_csv).to eq 'col1,2'
+    expect(subject.to_csv).to eq 'col1,2,3.00'
   end
 
   it 'should generate html' do
@@ -18,6 +38,7 @@ describe DataTable::DataTable do
       '<tr>'\
       '<td class="text">col1</td>'\
       '<td class="number">2</td>'\
+      '<td class="money">$3.00</td>'\
       '</tr>'\
       '</tbody>'\
       '</table>'
