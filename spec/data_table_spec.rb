be32e03ba243af04617e00cd91ae53e80337b198
Michael Noack <michael@noack.com.au>
2017-07-25 13:48:53 +0930

    Remove deprecations

--- spec/data_table_spec.rb
+++ spec/data_table_spec.rb
@@ -1,20 +1,26 @@
 require 'spec_helper'
 
 describe DataTable::DataTable do
+  let(:row) { ['col1', 2] }
+
   before do
-    @table = subject
-    row = ['col1', 2]
-    @table << {
-      :body => [row]
-    }
+    subject << { body: [row] }
   end
-  
+
   it 'should generate csv' do
-    @table.to_csv.should == 'col1,2'
+    expect(subject.to_csv).to eq 'col1,2'
   end
-  
+
   it 'should generate html' do
-    @table.to_html.should ==
-      '<table><tbody><tr><td class="text">col1</td><td class="number">2</td></tr></tbody></table>'
+    expect(subject.to_html).to eq(
+      '<table>'\
+      '<tbody>'\
+      '<tr>'\
+      '<td class="text">col1</td>'\
+      '<td class="number">2</td>'\
+      '</tr>'\
+      '</tbody>'\
+      '</table>'
+    )
   end
 end
